b95279a09e763092330d79b838a573eb6c264ae7
vanch <vanch@SUPERCOMPUTER>
2016-05-20 00:57:52 +0700

    add entity frameword code first for data module

---
+++ EShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        EShopDbContext db;
+        public EShopDbContext Init()
+        {
+            return db ?? (db = new EShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (db != null)
+            {
+                db.Dispose();
+            }
+        }
+    }
+}
