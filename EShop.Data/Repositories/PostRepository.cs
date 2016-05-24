4c7228564cab7bacdadf65843a8fee0599df2ab2
vanch <vanch@SUPERCOMPUTER>
2016-05-24 10:18:27 +0700

    commit more service

--- EShop.Data/Repositories/PostRepository.cs
+++ EShop.Data/Repositories/PostRepository.cs
@@ -10,7 +10,7 @@ namespace EShop.Data.Repositories
 {
     public interface IPostRepository : IRepository<Post>
     {
-
+        IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow);
     }
 
     public class PostRepository : RepositoryBase<Post>, IPostRepository
@@ -18,6 +18,18 @@ namespace EShop.Data.Repositories
         public PostRepository(IDbFactory dbFactory) : base(dbFactory)
         {
 
+        }
+
+        public IEnumerable<Post> GetAllByTag(string tag, int pageIndex, int pageSize, out int totalRow)
+        {
+            var model = from a in DbContext.Posts
+                        join b in DbContext.PostTags on a.ID equals b.PostID
+                        where b.TagID == tag && a.Status
+                        orderby a.CreatedDate descending
+                        select a;
+            totalRow = model.Count();
+            model = model.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            return model;
         }
     }
 }
--- EShop.Service/PostService.cs
+++ EShop.Service/PostService.cs
@@ -16,6 +16,7 @@ namespace EShop.Service
         void Delete(int id);
         IEnumerable<Post> GetAll();
         IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+        IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageSize, out int totalRow);
         Post GetById(int id);
         IEnumerable<Post> GetAllTagPaging(string tag, int page, int pageSize, out int totalRow);
         void SaveChanges();
@@ -54,9 +55,9 @@ namespace EShop.Service
             return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
         }
 
-        public IEnumerable<Post> GetAllTagPaging(string tag, int page, int pageSize, out int totalRow)
+        public IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow)
         {
-            return _postRepository.GetMultiPaging(x => x.Status,out totalRow, page, pageSize);
+            return _postRepository.GetAllByTag(tag, page, pageSize,out totalRow);
         }
 
         public Post GetById(int id)
@@ -72,6 +73,16 @@ namespace EShop.Service
         public void SaveChanges()
         {
             _unitOfWork.Commit();
+        }
+
+        public IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status && x.CategoryID == categoryId, out totalRow, page, pageSize, new string[] { "PostCategory" });
+        }
+
+        public IEnumerable<Post> GetAllTagPaging(string tag, int page, int pageSize, out int totalRow)
+        {
+            throw new NotImplementedException();
         }
     }
 }
