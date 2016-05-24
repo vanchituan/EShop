a9ff8d42bce65765d1c3eaee378502d473595212
vanch <vanch@SUPERCOMPUTER>
2016-05-24 16:46:30 +0700

    add unit test for repository and service

---
+++ EShop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,52 @@
+﻿using EShop.Data.Infrastructure;
+using EShop.Data.Repositories;
+using EShop.Model.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IUnitOfWork unitOfWork;
+        IPostCategoryRepository objRepository;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            unitOfWork = new UnitOfWork(dbFactory);
+            objRepository = new PostCategoryRepository(dbFactory);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objRepository.GetAll();
+            Assert.AreEqual(5, list.Count());
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory
+            {
+                Name = "post category",
+                Alias = "post-category",
+                Status = true
+            };
+
+            var result = objRepository.Add(category);
+            unitOfWork.Commit();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.ID);
+        }
+    }
+}
---
+++ EShop.UnitTest/ServiceTest/PostCategoryServiceTest.cs
@@ -0,0 +1,76 @@
+﻿using EShop.Data.Infrastructure;
+using EShop.Data.Repositories;
+using EShop.Model.Models;
+using EShop.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.UnitTest.ServiceTest
+{
+    [TestClass]
+    public class PostCategoryServiceTest
+    {
+        private Mock<IPostCategoryRepository> _mockRepository;
+        private Mock<IUnitOfWork> _mockUnitOfWork;
+        private IPostCategoryService _categoryService;
+        private List<PostCategory> _listCategory;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockRepository = new Mock<IPostCategoryRepository>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _categoryService = new PostCategoryService(_mockRepository.Object, _mockUnitOfWork.Object);
+            _listCategory = new List<PostCategory>
+            {
+                new PostCategory { ID = 1, Name ="mock1",Status =true},
+            new PostCategory { ID = 2, Name = "mock 1a", Status = true },
+            new PostCategory { ID = 3, Name = "mock 2", Status = true },
+            new PostCategory { ID = 4, Name = "mock 3", Status = true },
+            new PostCategory { ID = 5, Name = "mock 4", Status = true },
+            new PostCategory { ID = 6, Name = "mock 5", Status = true }
+            };
+        }
+
+        [TestMethod]
+        public void PostCategory_Service_GetAll()
+        {
+            //S1 : set up method
+            _mockRepository.Setup(m => m.GetAll(null)).Returns(_listCategory);
+            //S2 : call action
+            var result = _categoryService.GetAll() as List<PostCategory>;
+            //S3 : compare
+            Assert.IsNotNull(result);
+            Assert.AreEqual(6, result.Count);
+
+        }
+
+        [TestMethod]
+        public void PostCategory_Service_Create()
+        {
+            PostCategory category = new PostCategory();
+            int id = 1;
+            category.Name = "Test";
+            category.Alias = "test";
+            category.Status = true;
+
+            _mockRepository.Setup(m => m.Add(category)).Returns((PostCategory p) =>
+            {
+                p.ID = 1;
+                return p;
+            });
+
+            var result = _categoryService.Add(category);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.ID);
+        }
+
+
+    }
+}
