ccb8435bbefc4d50af0f643fa2ed21d65cc147cd
vanch <vanch@SUPERCOMPUTER>
2016-05-25 00:12:32 +0700

    automapper for web

--- EShop.Web/Global.asax.cs
+++ EShop.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EShop.Web.Mappings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,7 @@ namespace EShop.Web
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            AutoMapperConfiguration.ConFigure();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
---
+++ EShop.Web/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using EShop.Model.Models;
+using EShop.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EShop.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void ConFigure()
+        {
+            Mapper.CreateMap<Post, PostViewModel>();
+            Mapper.CreateMap<Tag, TagViewModel>();
+            Mapper.CreateMap<PostCategory, PostCategoryViewModel>();
+        }
+    }
+}
